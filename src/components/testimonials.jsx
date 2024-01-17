815372c9cc31884eb7a69494611129980a44fd11
Asha Chitroda <asha@nicatch.com>
2024-01-17 20:24:21 +0530

    dev-0: design changes and read more at testimonial container.

--- src/components/testimonials.jsx
+++ src/components/testimonials.jsx
@@ -1,31 +1,71 @@
 import React, { useState } from "react";
 import Slice from "../assets/testimonial/slice.jpg";
-import { BsPlayBtnFill } from "react-icons/bs";
+// import { BsPlayBtnFill } from "react-icons/bs";
 import { Modal } from "@mui/base";
-import { Box, CardMedia, IconButton, Typography } from "@mui/material";
+import {
+  Box,
+  CardMedia,
+  IconButton,
+  // Typography,
+  // useMediaQuery,
+} from "@mui/material";
 import video from "../assets/testimonial/slice-video.mov";
 import HighlightOffSharpIcon from "@mui/icons-material/HighlightOffSharp";
+import { useTheme } from "@emotion/react";
+import { FaPlay, FaPlayCircle } from "react-icons/fa";
 
 export const Testimonials = (props) => {
   const [open, setOpen] = useState(false);
+  // const [isOpenForReadMore, setIsOpenForReadMore] = useState(false);
 
   const style = {
     position: "fixed",
     top: "50%",
     left: "50%",
     transform: "translate(-50%, -50%)",
-    width: 800,
+    // width: 800,
     bgcolor: "background.paper",
-    border: "1px solid #0ec7c1",
+    border: "2px solid #0ec7c1",
     boxShadow: 24,
     p: 4,
+    marginTop: "70px",
   };
+
+  // const theme = useTheme();
+  // const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
 
   const handleOpen = () => {
     setOpen(true);
   };
   const handleClose = () => {
     setOpen(false);
+  };
+
+  // const handleIsMoreClose = () => {
+  //   setIsOpenForReadMore(false);
+  // };
+
+  const ReadMore = ({ children }) => {
+    console.log("children", children);
+    const text = children.props.children[1];
+    console.log("text", text);
+    const [isReadMore, setIsReadMore] = useState(true);
+    const toggleReadMore = () => {
+      // setIsOpenForReadMore(true);
+      setIsReadMore(!isReadMore);
+    };
+    return (
+      <p className="text">
+        {isReadMore ? text.slice(0, 100) : text}
+        <span
+          onClick={toggleReadMore}
+          className="read-or-hide"
+          style={{ color: "green" }}
+        >
+          {isReadMore ? "...read more" : " show less"}
+        </span>
+      </p>
+    );
   };
   return (
     <div id="testimonials">
@@ -63,32 +103,50 @@ export const Testimonials = (props) => {
                               left: "50%",
                               transform: "translate(-50%, -50%)",
                               textAlign: "center",
+                              border: "1px solid #f8b31e ",
+                              borderRadius: "50%",
+                              backgroundColor: "#f8b31e",
                             }}
                             s
                           >
-                            <BsPlayBtnFill
+                            <FaPlay
                               style={{
                                 fontSize: "3rem",
-                                color: "#e41a1a",
+                                color: "#df2838",
+                                padding: "6px",
+                                marginLeft: "2px",
                               }}
                             />
                           </div>
                         )}
                       </div>
                       <div className="testimonial-content">
-                        <p>"{d.text}"</p>
+                        <ReadMore text={d.text} key={`${d.text}-${i}`}>
+                          <p>"{d.text}"</p>
+                        </ReadMore>
                         <div className="testimonial-meta"> - {d.name} </div>
                       </div>
                       {open && d.video ? (
                         <Modal
+                          keepMounted
                           open={open}
                           onClose={handleClose}
                           aria-labelledby="modal-modal-title"
                           aria-describedby="modal-modal-description"
                           style={{
-                            position: "fixed",
-                            width: "50%",
-                            height: "50%",
+                            // position: "fixed",
+                            // width: "50%",
+                            // height: "50%",
+                            top: "25%",
+                            margin: "auto",
+                            // width: isSmallScreen ? "90%" : "50%",
+                            // height: isSmallScreen ? "80%" : "auto",
+                            // padding: isSmallScreen ? "20px" : "50px",
+                            // margin: isSmallScreen ? "10px" : "50px",
+                            backgroundColor: "white",
+                            display: "flex",
+                            flexDirection: "column",
+                            alignItems: "center",
                           }}
                         >
                           <Box sx={style}>
@@ -98,11 +156,11 @@ export const Testimonials = (props) => {
                               onClick={handleClose}
                               aria-label="close"
                               sx={{
-                                position: "absolute",
+                                position: "fixed",
                                 top: 0,
                                 right: 1,
-                                width: "20px",
-                                height: " 15px",
+                                width: "30px",
+                                height: " 25px",
                                 marginRight: "5px",
                                 marginTop: "5px",
                                 color: "red",
@@ -124,94 +182,63 @@ export const Testimonials = (props) => {
                       ) : (
                         <></>
                       )}
+                      {/* {isOpenForReadMore && d.text && (
+                        <Modal
+                          open={isOpenForReadMore}
+                          onClose={handleIsMoreClose}
+                          aria-labelledby="modal-modal-title"
+                          aria-describedby="modal-modal-description"
+                        >
+                          <Box sx={style}>
+                            <IconButton
+                              edge="end"
+                              color="inherit"
+                              onClick={handleIsMoreClose}
+                              aria-label="close"
+                              sx={{
+                                position: "absolute",
+                                top: 0,
+                                right: 1,
+                                width: "25px",
+                                height: " 15px",
+                                marginRight: "5px",
+                                marginTop: "5px",
+                                color: "red",
+                              }}
+                            >
+                              <HighlightOffSharpIcon />
+                            </IconButton>
+                            <Box sx={{ display: "flex" }}>
+                              <img src={d.img} alt="..." className="team-img" />
+                              <Box>
+                                <Typography
+                                  sx={{
+                                    mt: 2,
+                                    ml: 2,
+                                    fontSize: "15px",
+                                    color: "black",
+                                  }}
+                                >
+                                  {d.text}
+                                </Typography>
+                              </Box>
+                            </Box>
+                            <Typography
+                              id="modal-modal-description"
+                              sx={{ mt: 2 }}
+                            >
+                              {d.name}
+                            </Typography>
+                          </Box>
+                        </Modal>
+                      )} */}
                     </div>
                   </div>
                 );
               })
             : "loading"}
-          {/* <div className="col-md-4">
-            <di v className="testimonial">
-              <div
-                className="testimonial-image"
-                style={{ position: "relative", cursor: "pointer" }}
-                onClick={handleOpen}
-              >
-                {" "}
-                <img src={Slice} alt="" />{" "}
-                <div
-                  className="play-button"
-                  style={{
-                    position: "absolute",
-                    top: "50%",
-                    left: "50%",
-                    transform: "translate(-50%, -50%)",
-                    textAlign: "center",
-                  }}
-                  s
-                >
-                  <BsPlayBtnFill
-                    style={{
-                      fontSize: "3rem",
-                      color: "#e41a1a",
-                    }}
-                  />
-                </div>
-              </div>
-              <div className="testimonial-content">
-                <p>
-                  I just wanted to jump on and say a massive thank you to Nikhil
-                  and his team at Nicatch Private and particular Krupa. They've
-                  been nothing short of amazing when helping with the
-                  development of our slice platform, ensuring that they
-                  understand what business value looks like for us and
-                  delivering in a timely and cost effective manner. Absolutely,
-                  highly recommend them if you're looking for development
-                  services for your business. Thanks to Nicatch Private.
-                </p>
-                <div className="testimonial-meta"> - Amy stevens </div>
-              </div>
-            </di>
-          </div> */}
         </div>
       </div>
-      {/* {open && (
-        <Modal
-          open={open}
-          onClose={handleClose}
-          aria-labelledby="modal-modal-title"
-          aria-describedby="modal-modal-description"
-          style={{ position: "fixed", width: "50%", height: "50%" }}
-        >
-          <Box sx={style}>
-            <IconButton
-              edge="end"
-              color="inherit"
-              onClick={handleClose}
-              aria-label="close"
-              sx={{
-                position: "absolute",
-                top: 0,
-                right: 1,
-                width: "20px",
-                height: " 15px",
-                marginRight: "5px",
-                marginTop: "5px",
-                color: "red",
-              }}
-            >
-              <HighlightOffSharpIcon />
-            </IconButton>
-            <Box sx={{ display: "flex" }}>
-              <CardMedia
-                component="video"
-                src={d.video}
-                autoPlay
-                height="600"
-              />
-            </Box>
-          </Box>
-        </Modal>
-      )} */}
     </div>
   );
 };
